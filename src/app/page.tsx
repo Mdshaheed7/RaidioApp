22a0a16aa9eaa685f2f07a7a8f075122c681c630
Mdshaheed7 <boomboymds@gmail.com>
2024-11-04 01:12:41 +0530

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import RadioPlayer from '@/components/radio-player';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4 md:p-8 lg:p-24">
+      <RadioPlayer />
+    </main>
+  );
+}
